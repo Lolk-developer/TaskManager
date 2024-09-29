121edade1c28e306998048a9e9089624352d134c
ypOnS <165901543+YpOnS@users.noreply.github.com>
2024-09-29 18:04:15 +0500

    End task Update for main.py
    Update at 29.09.2024 for Main.py. I(YpoNs) add def end_task for end 
    function.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import tkinter
 from tkinter import *
 from tkinter import ttk
 from tkinter.messagebox import showerror
 #import os
-
+archiv_task=None
 def windown1(): #Напоминания
     Win1 = Tk()
     Win1.title("Напоминания")
@@ -16,8 +17,7 @@ def windown1(): #Напоминания
             Tasklist.insert(0, task)
         else:
             showerror(title="Пустое поле.", message="Пожалуйста, введите напоминание.")
-
-     def delete_task():
+    def delete_task():
        global tasks #а зачем?
        task=Tasklist.curselection()
        if task:
@@ -25,6 +25,14 @@ def windown1(): #Напоминания
        else:
           showerror(title="Пустое поле.", message="Пожалуйста, ввыберете упоминание!")
 
+    def end_task():
+        task = Tasklist.curselection()
+        if task:
+            select_task=Tasklist.get(task)
+            archiv_task.insert(END, select_task)
+            Tasklist.delete(task)
+        else:
+            showerror("Ничего не выбрано", "Выберите что-то")
 
     Createtask = ttk.Entry(Win1)
     Createtask.place(rely=0, relx=0, relwidth=1)
@@ -35,22 +43,25 @@ def windown1(): #Напоминания
     Editbtn1 = ttk.Button(Win1, text="Редактировать")
     Editbtn1.place(relx=0.175, rely=0.05)
 
-    Endbtn1 = ttk.Button(Win1, text="Завершить")
-    Endbtn1.place(relx=0.42, rely=0.05)
+    Endbtn1 = ttk.Button(Win1, text="Завершить", command=end_task)
+    Endbtn1.place(relx=0.4, rely=0.05)
 
     Delbtn = ttk.Button(Win1, text="Удалить", command=delete_task)
     Delbtn.place(relx=0.625, rely=0.05)
 
-    Archivebtn = ttk.Button(Win1, text="Архив задач")
+    Archivebtn = ttk.Button(Win1, text="Архив задач",)
     Archivebtn.place(relx=0.8, rely=0.05)
 
     Tasklist = Listbox(Win1)
-    Tasklist.place(relx=0, rely=0.15, relwidth=1, relheight=0.9)
+    Tasklist.place(relx=0.1, rely=0.3)
+
+    archiv_task = Listbox(Win1)
+    archiv_task.place(relx=0.5, rely=0.3)
+
+
 
     #Backbtn = ttk.Button(Win1, text="<- back")
     #Backbtn.placepack(anchor=NW)
-
-
 
 Mainwindow = Tk()
 Mainwindow.title("Notes") #Титл
@@ -58,9 +69,7 @@ Mainwindow.geometry("600x710") #Разрешние
 Mainwindow.resizable(False, False)
 
 tasks = []
-
 #windows
-
 Text = ttk.Label(text="Tast Manager", font=("Helvetica", 16))
 Text.place(relx=0.426, rely=0.35)
 
